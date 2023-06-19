93a6e3dfe9bfddfa9024ef7dad8d420784d0516e
zTraxDev <josemarinez243@gmail.com>
2023-06-20 00:34:50 +0200

    Change for DB

--- src/db/client.py
+++ src/db/client.py
@@ -1,10 +1,17 @@
-import pymongo
+from pymongo import MongoClient
+from src.models.Product_Model import Product
 
-try:
-    client = pymongo.MongoClient('localhost', 27017)
-    db = client['my_database']
-    users_collection = db['users']
-    products_collection = db['products']
-    print(f"Database Load ✔️  | PyMongo: {pymongo.__version__}")
-except Exception as e:
-     print(f"Error al Conectar {e}")
+class DataBase:
+    def __init__(self, URI: str, database: str, collection: str):
+        self.client = MongoClient(database)
+        self.db = self.client[database]
+        self.collection = self.db[collection]
+
+    def show_all_item(self):
+        return self.collection.find()
+    
+    def show_item(self, id):
+        return self.collection.find_one({"_id": id})
+    
+    def create_item(self, item: Product):
+        return self.collection.insert_one(item).inserted_id
--- src/routes/products.py
+++ src/routes/products.py
@@ -1,36 +1,38 @@
 from fastapi import APIRouter, HTTPException, status
-from src.db.client import products_collection, users_collection
+from src.db.client import DataBase
 from bson import ObjectId
 from src.models.Product_Model import Product
 from src.middleware.functions import productEntity, show_all_products
-from src.db.db_function import *
+
 
 router = APIRouter(prefix="/products")
+db = DataBase("localhost", "crud", "products")
+cur = db.collection
 
 @router.get("/")
 async def view_products():
-    return show_all_products(products_collection.find())
+    return show_all_products(db.show_all_item())
 
 
 @router.post("/create", status_code=status.HTTP_201_CREATED)
 async def create_item(product: Product):
    new_item = dict(product)
    name = new_item.get('name')
-   query = products_collection.find_one({"name": name})
+   query = cur.find_one({"name": name})
    
    if query is not None:
        raise HTTPException(status.HTTP_404_NOT_FOUND, "User is Create")
    
-   id = products_collection.insert_one(new_item).inserted_id
-   query = products_collection.find_one({"_id": id})
+   id = cur.insert_one(new_item).inserted_id
+   query = cur.find_one({"_id": id})
    return productEntity(query)
-products_collection.aggregate
+
 
 
 @router.delete("/{id}")
 async def delete_item(id: str):
-    query = products_collection.find_one({"_id": ObjectId(id)})
+    query = cur.find_one({"_id": ObjectId(id)})
     if query is None:
         raise HTTPException(status.HTTP_404_NOT_FOUND, "Producto No encontrado")
-    products_collection.find_one_and_delete({"_id": ObjectId(id)})
+    cur.find_one_and_delete({"_id": ObjectId(id)})
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product Delete Successful")
